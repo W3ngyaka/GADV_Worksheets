6b894652109ceaa736fa6ba4ccad42b502e4200e
JunHeng <junhengweng@gmail.com>
2025-07-14 15:52:53 +0800

    week 6
    Intantiating GameObjects

---
+++ Assets/06 Runtime Scipting, C# generics/Instantiating GameObjects/Scripts/ObjectSpawner.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject AsteroidPrefab;
+
+    void Start()
+    {
+        GameObject newAsteroid = Instantiate(
+            AsteroidPrefab,
+            new Vector3(0, 0, 0),
+            Quaternion.identity
+        );
+    }
+}
